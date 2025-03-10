431f0eb005ff2b68593c82c7ad19ce100f45d117
Harsh Raj <hraj@qodeleaf.com>
2025-03-11 00:49:26 +0530

    added firebase authentication

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -3,13 +3,14 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDktK8MNWxTlxZ3kHwvkU_n3CbjbVQb9Zg",
+  apiKey: import.meta.env.VITE_apiKey,
   authDomain: "netflixgpt-193ab.firebaseapp.com",
   projectId: "netflixgpt-193ab",
   storageBucket: "netflixgpt-193ab.firebasestorage.app",
